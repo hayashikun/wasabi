9ac3c25a0a1a4a8effce57c4cba490ac7119eedc
Ryosuke Hayashi <hayashi@hayashikun.com>
2022-05-05 00:29:35 +0900

    add app that save image

---
+++ proxy-wasm/app/app.py
@@ -0,0 +1,18 @@
+import flask
+import time
+
+app = flask.Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def index():
+    data = flask.request.data
+    with open(f"/tmp/{time.time()}.jpg", 'wb') as f:
+        print(f)
+        r = f.write(data)
+        print(r)
+
+    return 'ok'
+
+
+app.run(host='0.0.0.0', port=8000)
