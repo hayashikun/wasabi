781593b12ae5fc3abd379624706d8e77ed3f31f9
Ryosuke Hayashi <hayashi@hayashikun.com>
2022-05-07 12:08:32 +0900

    fix

--- proxy-wasm/app/app.py
+++ proxy-wasm/app/app.py
@@ -1,5 +1,5 @@
 import flask
-import time
+# import time
 
 app = flask.Flask(__name__)
 
@@ -7,12 +7,14 @@ app = flask.Flask(__name__)
 @app.route('/', methods=['POST'])
 def index():
     data = flask.request.data
+    """
     with open(f"/tmp/{time.time()}.jpg", 'wb') as f:
         print(f)
         r = f.write(data)
         print(r)
+    """
 
-    return 'ok'
+    return f"size:  {len(data)}"
 
 
 app.run(host='0.0.0.0', port=8000)
